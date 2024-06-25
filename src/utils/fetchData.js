15cce24f6ba9a876a0ff7753ef51e533d9bfb7dd
vdat362002 <vandatdinh2@gmail.com>
2024-06-25 21:25:56 +0700

    fix

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+axios.defaults.baseURL = process.env.REACT_APP_SocialMedia_URL;
+
 export const getDataAPI = async(url, accessToken) => {
   const res = await axios.get(`/api/v1/${url}`, {
     headers: {
